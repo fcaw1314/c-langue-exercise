fef9e4d49d501b367d95eb40af17ff0367e2fe52
fcawyxb <2377423295@qq.com>
2023-02-04 15:04:16 +0800

    study

---
+++ C语言进阶/2-4.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）。
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int money = 0;
+//	int total = 0;
+//	int empty = 0;
+//	scanf("%d", &money);
+//	total = money;
+//	empty = money;
+//	while (empty >= 2)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("%d\n", total);
+//	return 0;
+//}
+
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hello bit\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+//题目：
+//输入一个整数数组，实现一个函数，
+//来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+//
+//void print(int arr[], int sz)
+//{
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//void move_even_odd(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	//判断偶数
+//	while (left < right)
+//	{
+//		//防止越界
+//		while ((left < right)&&(arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		//判断基数
+//		while ((left < right)&&(arr[right] % 2 == 0))
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//			left++;
+//			right--;
+//		}
+//		
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	move_even_odd(arr, sz);
+//	print(arr, sz);
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
